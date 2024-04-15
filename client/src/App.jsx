2b45ce8a3cc099abe395f43ef15ab1a89639fbb1
Yebin <yebinp1102@naver.com>
2024-04-15 15:45:47 +0900

    Feat: community main page layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import { Route, Routes} from 'react-router-dom';
-import Home from './pages/home/Home';
 import RootLayout from './pages/RootLayout';
+import Home from './pages/root/Home';
+import Community from './pages/root/Community';
+import CommunityNewPost from './pages/root/community/CommunityNewPost';
 
 const App = () => {
   return (
@@ -8,6 +10,10 @@ const App = () => {
       <Routes>
         <Route element={<RootLayout />}>
           <Route path='/' element={<Home />} />
+          {/* community */}
+          <Route path='/community' element={<Community />} />
+          <Route path='/community/write' element={<CommunityNewPost />} />
+
         </Route>
       </Routes>
     </>
---
+++ client/src/pages/root/community/CommunityNewPost.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CommunityNewPost = () => {
+  return (
+    <div>CommunityNewPost</div>
+  )
+}
+
+export default CommunityNewPost
