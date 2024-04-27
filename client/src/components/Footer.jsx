6a3b615c7badfaa547a1b6399e0bd3aefd670757
yebin <yebinp1102@naver.com>
2024-04-27 23:28:53 +0900

    Feat: Home page banner

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import '../style/footer.css';
+
+const Footer = () => {
+  return (
+    <div className='footer-wrap'>
+      <div className="footer-container">
+        hi
+      </div>
+    </div>
+  )
+}
+
+export default Footer
